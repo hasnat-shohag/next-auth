48fea97ba1ff64aa114e63a08e8e9b1aab9f46dd
Hasnat Shohag <hasnat.ru.ice19@gmail.com>
2024-12-30 15:38:04 +0600

    csrf token successfull

--- src/components/auth/signin-form.tsx
+++ src/components/auth/signin-form.tsx
@@ -1,44 +1,52 @@
 "use client"
 
-import { requestSignIn } from "@/actions/request-sign-in";
-import { useEffect } from "react";
-import { useFormState } from "react-dom";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
 
+export const SignInForm = ({ csrfToken }: {csrfToken: string}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signIn("credentials", {
+      email,
+      password,
+      callbackUrl: "/",
+    });
+  };
 
-
-const initState = {error: ''};
-
-export const SignInForm = ({ csrfToken }: { csrfToken?: string}) => {
-	const [fstate,action] = useFormState(requestSignIn,initState);
-
-	useEffect(()=>{
-    if (fstate) {
-      const { error } = fstate;
-			
-      if (error) alert(error);
-    }
-  }, [fstate])
-
-	return (
-
-		<form action={action} className="flex flex-col gap-6 shadow-md border-1 bg-gray-50 w-1/2 xl:w-1/4 p-6 rounded-md" >
-			{csrfToken && <input type="hidden" name="csrfToken" value={csrfToken} />}
-			
-			<label className="flex justify-between items-center">
-				Email
-				<input name="email" type="email" placeholder="Enter email" 
-					className="w-3/4 p-3 rounded-md outline outline-1"
-				/>
-			</label>
-			
-			<label className="flex justify-between items-center">
-				Password
-				<input name="password" type="password" placeholder="Enter password"
-					className="w-3/4 p-3 rounded-md outline outline-1"
-				/>
-			</label>
-			<button type="submit" className="py-3 rounded-md outline outline-1 hover:bg-gray-100">Sign In</button>
-		</form>
-	)
-}
+  return (
+    <form onSubmit={handleSubmit} method="POST" className="flex flex-col gap-6 shadow-md border-1 bg-gray-50 w-1/2 xl:w-1/4 p-6 rounded-md">
+      {csrfToken && <input type="hidden" name="csrfToken" value={csrfToken} />}
+      
+      <label className="flex justify-between items-center">
+        Email
+        <input
+          name="email"
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-3/4 p-3 rounded-md outline outline-1"
+        />
+      </label>
+      
+      <label className="flex justify-between items-center">
+        Password
+        <input
+          name="password"
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-3/4 p-3 rounded-md outline outline-1"
+        />
+      </label>
+      
+      <button type="submit" className="py-3 rounded-md outline outline-1 hover:bg-gray-100">
+        Sign In
+      </button>
+    </form>
+  );
+};
