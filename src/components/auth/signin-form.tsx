f69cde50c9d610436041e7643a85cd6a661f779d
Hasnat Shohag <hasnat.ru.ice19@gmail.com>
2025-01-03 15:19:01 +0600

    add proper type for form submit handler

--- src/components/auth/signin-form.tsx
+++ src/components/auth/signin-form.tsx
@@ -7,7 +7,7 @@ export const SignInForm = ({ csrfToken }: {csrfToken: string}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     await signIn("credentials", {
       email,
